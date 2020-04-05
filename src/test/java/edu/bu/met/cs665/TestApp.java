594216dcf639d0fdb3c4d2e79697371b7bb22264
Karl Lamoureux <lamoureuxk@wit.edu>
2020-04-05 12:41:45 -0400

    All Tests Complete

--- src/test/java/edu/bu/met/cs665/TestApp.java
+++ src/test/java/edu/bu/met/cs665/TestApp.java
@@ -5,17 +5,63 @@ import org.junit.Before;
 import org.junit.Test;
 import encryption.Encryption;
 import emailQuality.SpellChecker;
+import customer.*;
 
 public class TestApp {
 	
 	private static EmailGenerationSystem gen;
+	private static final String EXAMPLE_BODY = "This is an example body.\nThis tests email generation.";
+	private static final String EXAMPLE_NAME = "Example Sender";
 	
 	@Before
 	public void initialize() {
 		gen = new EmailGenerationSystem();
 	}
 	
+	@Test
+	public void testVIPEmailGeneration() {
+		Customer vip = new VIPcustomer();
+		String email = gen.generateEmail(vip, EXAMPLE_BODY, EXAMPLE_NAME);
+		assertTrue(email.contains(gen.VIP_HEADER));
+		assertTrue(email.contains(gen.VIP_FOOTER + EXAMPLE_NAME));
+		assertTrue(email.contains(EXAMPLE_BODY));
+	}
 	
+	@Test
+	public void testFrequentEmailGeneration() {
+		Customer Frequent = new FrequentCustomer();
+		String email = gen.generateEmail(Frequent, EXAMPLE_BODY, EXAMPLE_NAME);
+		assertTrue(email.contains(gen.FREQUENT_HEADER));
+		assertTrue(email.contains(gen.FREQUENT_FOOTER + EXAMPLE_NAME));
+		assertTrue(email.contains(EXAMPLE_BODY));
+	}
+	
+	@Test
+	public void testBusinessEmailGeneration() {
+		Customer Business = new BusinessCustomer();
+		String email = gen.generateEmail(Business, EXAMPLE_BODY, EXAMPLE_NAME);
+		assertTrue(email.contains(gen.BUSINESS_HEADER));
+		assertTrue(email.contains(gen.BUSINESS_FOOTER + EXAMPLE_NAME));
+		assertTrue(email.contains(EXAMPLE_BODY));
+	}
+	
+	@Test
+	public void testNewEmailGeneration() {
+		Customer New = new NewCustomer();
+		String email = gen.generateEmail(New, EXAMPLE_BODY, EXAMPLE_NAME);
+		assertTrue(email.contains(gen.NEW_HEADER));
+		assertTrue(email.contains(gen.NEW_FOOTER + EXAMPLE_NAME));
+		assertTrue(email.contains(EXAMPLE_BODY));
+	}
+	
+	@Test
+	public void testReturningEmailGeneration() {
+		Customer Returning = new ReturningCustomer();
+		String email = gen.generateEmail(Returning, EXAMPLE_BODY, EXAMPLE_NAME);
+		assertTrue(email.contains(gen.RETURNING_HEADER));
+		assertTrue(email.contains(gen.RETURNING_FOOTER + EXAMPLE_NAME));
+		assertTrue(email.contains(EXAMPLE_BODY));
+	}
 	
 	@Test
 	public void testSpellCheck() {
